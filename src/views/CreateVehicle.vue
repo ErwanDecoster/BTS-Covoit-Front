9370ddca1f041cc38a8bc09a664c6d007522e62a
Erwan Decoster <e.decoster@sio.local>
2022-02-11 17:44:07 +0100

    debut recup data in database

--- src/views/CreateVehicle.vue
+++ src/views/CreateVehicle.vue
@@ -16,6 +16,9 @@
           <label class="text-left font-bold" for="motorisation">Motorisation : </label>
           <input class="drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] rounded-full p-2 text-lg" type="text" name="motorisation" id="motorisation">
         </div>
+        <ul>
+          <li v-for="row in allData" v-bind:key="row.motorization"></li>
+        </ul>
         <div class="grid gap-2">
           <label class="text-left font-bold" for="number_of_place">Nombres de places : </label>
           <input class="drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] rounded-full p-2 text-lg" type="number" max="6" min="1" name="number_of_place" id="number_of_place">
@@ -36,5 +39,12 @@ import Navbar from '@/components/Navbar.vue';
 
 export default {
   components: { Navbar },
+  data() {
+    return {
+      allData: '',
+    };
+  },
+  methods: {
+  },
 };
 </script>
